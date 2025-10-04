e9699a8b5a8ba0e780c4bb4c689a5d191329a302
aniketkale11 <kale25289@gmail.com>
2025-10-04 13:29:44 +0530

    update

--- blog.py
+++ blog.py
@@ -2,7 +2,14 @@ from fastapi import APIRouter , Depends , Request , Form
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
 from database import get_data
-from models import Comment , User , Post , Like
+from sqlalchemy import func
+from models import Comment , User , Post , Like , Follow
+from fastapi import File , UploadFile 
+import shutil
+from sqlalchemy import or_
+from sqlalchemy.orm import joinedload 
+import os
+from datetime import datetime
 from fastapi.responses import RedirectResponse
 
 router = APIRouter ( )
@@ -10,8 +17,25 @@ router = APIRouter ( )
 
 
 templates = Jinja2Templates(directory="templates")
-
+UPLOAD_FOLDER = "static/uploads"
+os.makedirs(UPLOAD_FOLDER , exist_ok=True)
 #Show all Post
+
+
+
+def highlight_text (text:str , query:str):
+    if not query :
+        return text
+    
+    import re 
+    pattern  = re.compile(re.escape(query),re.IGNORECASE)
+
+    return pattern.sub(lambda m: f"<mark>{m.group(0)}</mark>",text)
+
+
+
+
+
 
 @router.get('/')
 async def home (
@@ -28,20 +52,108 @@ async def home (
         request.session.clear()
         return templates.TemplateResponse("new.html", {"request": request})
 
-
+"""
 @router.get('/home')
 async def get_post(
+    
     request:Request,
     db:Session = Depends(get_data)
 ):
     user_id = request.session.get('user_id')
     posts = db.query(Post).all()
+   # images = db.query(Post).all()
     user = db.query(User).filter(User.id == user_id).first()
+
     return templates.TemplateResponse(
         'post.html',
         {'request':request,
         "posts":posts,"user":user}
     )
+"""
+
+
+@router.get('/home')
+async def get_post(
+    request: Request,
+    db: Session = Depends(get_data)
+):
+    user_id = request.session.get('user_id')
+    user = db.query(User).filter(User.id == user_id).first()
+    search_query = request.query_params.get('q') # get search query
+    posts_query = db.query(Post)
+   
+
+    if search_query:
+        posts_query  = posts_query.filter(
+            or_(
+                Post.title.ilike(f"%{search_query}%"),
+                Post.content.ilike(f"%{search_query}%"),
+                #User.username.ilike(f"%{search_query}%")
+            )
+        )
+
+
+    posts = posts_query.all()
+
+
+    #followers_count = db.query(Follow).filter_by(following_id=post.owner_id).count()
+
+
+    # प्रत्येक post साठी current user follow करतो का ते तपासा
+    posts_with_status = []
+    for post in posts:
+        is_following = db.query(Follow).filter_by(
+            follower_id=user_id,
+            following_id=post.owner_id
+        ).first() is not None
+
+        followers_count = db.query(Follow).filter_by(following_id=post.owner_id).count()
+        
+        highlight_title = post.title
+        highlight_content = post.content
+
+
+        if search_query:
+            highlight_title = highlight_text(post.title, search_query)
+            highlight_content = highlight_text(post.content , search_query)
+ 
+        
+
+
+
+
+        posts_with_status.append({
+            "post": post,
+            "is_following": is_following,
+            "followers_count":followers_count,
+            "highlight_title":highlight_title,
+            "highlight_content":highlight_content
+            
+        })
+    user_followers_count = 0
+    if user:
+        user_followers_count = db.query(Follow).filter_by(following_id=user.id).count()
+    
+
+    
+    if user:
+        followers = (
+            db.query(User.username)
+            .join(Follow, Follow.follower_id == User.id)
+            .filter(Follow.following_id == user.id)
+            .all()
+        )
+        followers_list = [f[0] for f in followers]
+    else:
+     followers_list = [ ]
+
+    
+    return templates.TemplateResponse(
+        "post.html",
+        {"request": request, "posts": posts_with_status, "user": user,"followers_count":user_followers_count,
+         "follow":followers_list}
+    )
+
 
 #show single or particultar post 
 
@@ -88,7 +200,6 @@ async def like_post(
         Like.user_id == user_id , Like.post_id == post_id
     ).first()
 
-
     if existing_like :
         db.delete(existing_like)
         db.commit()
@@ -102,7 +213,36 @@ async def like_post(
     return RedirectResponse(url=f"/home#post-{post.id}",status_code=303)
 
 
+@router.post('/follow/{user_id}')
+async def follow_user(
+    user_id:int,
+    request:Request,
+    db:Session = Depends(get_data)
+):
+    follower_id = request.session.get("user_id")
 
+    if not follower_id:
+        request.session.clear()
+        return RedirectResponse(url="/login",status_code=303)
+    
+    if follower_id == user_id:
+       return RedirectResponse(url="/home", status_code=303)
+    
+
+    existing = db.query(Follow).filter_by(follower_id=follower_id, following_id=user_id).first()
+    if existing:
+        db.delete(existing)
+        db.commit()
+
+
+    else :
+        follow = Follow(follower_id=follower_id, following_id=user_id)
+        db.add(follow)
+        db.commit()
+    
+
+    return RedirectResponse(url=request.headers.get("referer"), status_code=303)  
+    
 
 
 
@@ -112,6 +252,7 @@ async def  create_post(
     request:Request,
     title:str = Form(None),
     content:str = Form(None),
+    image: UploadFile = File(None),
     db:Session =Depends(get_data)
 ):
     if 'user_id' not in request.session:
@@ -129,8 +270,16 @@ async def  create_post(
     #if 'user_id' not in request.session:
      #   return RedirectResponse(url='/login',status_code=303)
     
+    filename = None
+    if image:
+       # allowed_extensions = [".jpg", ".jpeg", ".png", ".gif", ".mp4", ".mov", ".avi"]
+       # ext = os.path.splitext(image.filename)[1].lower()
+        filename = f"{int(datetime.utcnow().timestamp())}_{image.filename}"
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(image.file, buffer)
 
-    new_post = Post(title = title , content = content , owner_id = request.session['user_id'])
+    new_post = Post(title = title , content = content , owner_id = request.session['user_id'],image = filename )
     db.add(new_post)
     db.commit()
     request.session['post_id'] = new_post.id
